dfcd38da1d61ddb22dc96abfe799b12448e4f180
J1 <130596532+J9214@users.noreply.github.com>
2025-06-09 12:30:28 +0900

    [Bronze IV] Title: 가희와 방어율 무시, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25238. 가희와 방어율 무시/가희와 방어율 무시.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+int main(){
+    FASTIO
+
+    int a,b; cin >> a >> b;
+    cout << (a*(100-b) <10000);
+}
