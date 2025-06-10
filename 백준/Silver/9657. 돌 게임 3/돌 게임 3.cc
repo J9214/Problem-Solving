985b2d9799ee0876b24b9df894a506af7556ede5
J1 <130596532+J9214@users.noreply.github.com>
2025-06-10 17:38:48 +0900

    [Silver III] Title: 돌 게임 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9657. 돌 게임 3/돌 게임 3.cc
+++ 백준/Silver/9657. 돌 게임 3/돌 게임 3.cc
@@ -1,32 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-typedef long long ll;
-typedef pair<int, int> pi;
-typedef pair<ll, ll> pl;
-typedef vector<int> vi;
-typedef vector<ll> vll;
-typedef vector<vector<int>> vvi;
-typedef vector<vector<ll>> vvll;
-typedef vector<pair<int, int>> vpi;
-typedef vector<pair<ll, ll>> vpll;
-typedef struct Point {int x, y;} point;
-point direction[4] = {{1,0},{0,1},{-1,0},{0,-1}};
-#define X first
-#define Y second
-void print(vvi mat){for(auto i : mat){for(auto j : i) cout << j << ' ';cout << '\n';}}
-void print(vi vec){for(auto i : vec) cout << i << ' ';}
-#define modulo 1000000007
 
 int main(){
     FASTIO
 
     int n; cin >> n;
-    vi vec(n+1,0);
-    vec[1]=vec[3]=vec[4] = 1;
-
-    for(int i = 5 ; i <= n ; i++)
-        if(!(vec[i-4] && vec[i-3] && vec[i-1])) vec[i]=1;
-    
-    cout << (vec[n]?"SK":"CY");
+    cout << (n%7==0||n%7==2?"CY":"SK");
 }
