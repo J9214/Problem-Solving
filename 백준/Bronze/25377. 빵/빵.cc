6ed6ac6307706675128592c54b8b5cf455c61d3a
J1 <130596532+J9214@users.noreply.github.com>
2025-06-11 10:54:34 +0900

    [Bronze IV] Title: 빵, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/25377. 빵/빵.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<ll>> vvll;
+typedef vector<pair<int, int>> vpi;
+typedef vector<pair<ll, ll>> vpll;
+typedef struct Point {int x, y;} point;
+point direction[4] = {{1,0},{0,1},{-1,0},{0,-1}};
+#define X first
+#define Y second
+void print(vvi mat){for(auto i : mat){for(auto j : i) cout << j << ' ';cout << '\n';}}
+void print(vi vec){for(auto i : vec) cout << i << ' ';}
+#define modulo 1000000007
+
+int main(){
+    FASTIO
+
+    int n; cin >> n;
+    int res= modulo;
+    while(n--){
+        int a,b;cin >> a >> b;
+        if(a<=b) res = min(res,b);
+    }
+    cout << (res==modulo?-1:res);
+}
