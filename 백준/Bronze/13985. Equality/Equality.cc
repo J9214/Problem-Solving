6dda1262c7348cfb4688d79c8c702b0fe38051a2
J1 <130596532+J9214@users.noreply.github.com>
2025-06-19 12:07:25 +0900

    [Bronze IV] Title: Equality, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/13985. Equality/Equality.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+int main(){
+    FASTIO
+
+    int a,b,c;char q, w;
+    cin >> a >> q >> b >> w >> c;
+    cout << (c==a+b?"YES":"NO");
+}
