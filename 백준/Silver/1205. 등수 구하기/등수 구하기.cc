6a6c50d0c27839d38b9df5192104a7cdf1246a8c
J1 <130596532+J9214@users.noreply.github.com>
2025-06-24 15:06:18 +0900

    [Silver IV] Title: 등수 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1205. 등수 구하기/등수 구하기.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<ll>> vvll;
+typedef vector<pair<int, int>> vpi;
+typedef vector<pair<ll, ll>> vpll;
+typedef struct Point {int x, y;} point;
+point direction[4] = {{1,0},{0,1},{-1,0},{0,-1}};
+#define X first
+#define Y second
+void print(vvi mat){for(auto i : mat){for(auto j : i) cout << j << ' ';cout << '\n';}}
+void print(vi vec){for(auto i : vec) cout << i << ' ';}
+#define modulo 1000000007
+
+int main(){
+    FASTIO
+
+    int n,score,p; cin >> n >> score >> p;
+    vi vec(n);
+    for(int i = 0; i< n; i++)cin >> vec[i];
+    if(upper_bound(vec.begin(), vec.end(), score, greater<int>())-vec.begin()>=p) cout << -1;
+    else cout << lower_bound(vec.begin(), vec.end(), score, greater<int>())-vec.begin()+1;
+}
