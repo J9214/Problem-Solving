d3b229e3082cdb8dd4d3c441f557f7a70543fc84
J1 <130596532+J9214@users.noreply.github.com>
2025-07-11 01:00:04 +0900

    [Bronze III] Title: 등장하지 않는 문자의 합, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/3059. 등장하지 않는 문자의 합/등장하지 않는 문자의 합.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<ll>> vvll;
+typedef vector<pair<int, int>> vpi;
+typedef vector<pair<ll, ll>> vpll;
+typedef struct Point {int x, y;} point;
+point direction[4] = {{1,0},{0,1},{-1,0},{0,-1}};
+#define X first
+#define Y second
+void print(vvi mat){for(auto i : mat){for(auto j : i) cout << j << ' ';cout << '\n';}}
+void print(vi vec){for(auto i : vec) cout << i << ' ';}
+#define modulo 1000000007
+
+int main(){
+    FASTIO
+
+    int n; cin >> n;
+    while(n--){
+        string s; cin >> s;
+        vi vec(26,0);
+        int res=0;
+        for(auto str : s)vec[str-'A']=1;
+        for(int i = 0 ; i < vec.size(); i++){
+            if(!vec[i]) res+=i+'A';
+        }
+        cout << res << '\n';
+    }
+}
