8f0d0e134b6862e4d119d950db0a1e32358daf5d
J1 <130596532+J9214@users.noreply.github.com>
2025-07-18 12:31:19 +0900

    [Bronze IV] Title: 지폐 세기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30031. 지폐 세기/지폐 세기.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<ll>> vvll;
+typedef vector<pair<int, int>> vpi;
+typedef vector<pair<ll, ll>> vpll;
+typedef struct Point {int x, y;} point;
+point direction[4] = {{1,0},{0,1},{-1,0},{0,-1}};
+#define X first
+#define Y second
+void print(vvi mat){for(auto i : mat){for(auto j : i) cout << j << ' ';cout << '\n';}}
+void print(vi vec){for(auto i : vec) cout << i << ' ';}
+#define modulo 1000000007
+
+int main(){
+    FASTIO
+
+    int n; cin >> n;
+    int res=0;
+    while(n--){
+        int a,b;cin >> a >>b;
+        if(a==136)a=1000;
+        else if(a==142)a=5000;
+        else if(a==148)a=10000;
+        else a=50000;
+        res += a;
+    }
+    cout << res;
+}
