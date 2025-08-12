bef2611d2153f27075ac79127eb6ab63bae4fb3f
J1 <130596532+J9214@users.noreply.github.com>
2025-08-12 21:02:11 +0900

    [Bronze III] Title: 페르시아의 왕들, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10599. 페르시아의 왕들/페르시아의 왕들.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<ll>> vvll;
+typedef vector<pair<int, int>> vpi;
+typedef vector<pair<ll, ll>> vpll;
+typedef struct Point {int x, y;} point;
+point direction[4] = {{1,0},{0,1},{-1,0},{0,-1}};
+#define modulo 1000000007
+void print(vvi mat){for(auto i : mat){for(auto j : i) cout << j << ' ';cout << '\n';}}
+void print(vi vec){for(auto i : vec) cout << i << ' ';}
+
+int main(){
+    FASTIO
+    
+    int a,b,c,d;
+    while(cin >> a >> b >> c >> d){
+        if(!a&&!b&&!c&&!d) break;
+        cout << c-b << ' ' << d-a << '\n';
+    }
+}
