32728628a5eb157c961c27d0804604380b3c2c28
J1 <130596532+J9214@users.noreply.github.com>
2025-08-17 19:55:39 +0900

    [Bronze V] Title: 사랑은 고려대입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+int main(){
+    FASTIO
+
+    int n; cin >> n;
+    while(n--)
+        cout << "LoveisKoreaUniversity ";
+}
