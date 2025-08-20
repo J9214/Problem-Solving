bf8fa24d40b71a82aab3b4e4527104efc1567313
J1 <130596532+J9214@users.noreply.github.com>
2025-08-20 17:44:28 +0900

    [Bronze V] Title: AI 선도대학, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32951. AI 선도대학/AI 선도대학.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+int main(){
+    FASTIO
+
+    int n; cin >> n;
+    cout << n-2024;
+}
