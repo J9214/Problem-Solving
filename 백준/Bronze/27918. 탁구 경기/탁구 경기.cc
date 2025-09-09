10ee7ae1f9244cae5c000f9b86216e95c1eb6a6a
J1 <130596532+J9214@users.noreply.github.com>
2025-09-09 18:43:25 +0900

    [Bronze IV] Title: 탁구 경기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27918. 탁구 경기/탁구 경기.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<ll>> vvll;
+typedef vector<pair<int, int>> vpi;
+typedef vector<pair<ll, ll>> vpll;
+typedef struct Point {int x, y;} point;
+point direction[4] = {{1,0},{0,1},{-1,0},{0,-1}};
+#define modulo 1000000007
+void print(vvi mat){for(auto i : mat){for(auto j : i) cout << j << ' ';cout << '\n';}}
+void print(vi vec){for(auto i : vec) cout << i << ' ';}
+
+int main(){
+    FASTIO
+    
+    vi vec(2);
+    int n; cin >> n;
+    while(n--){
+        char c; cin >> c;
+        if(c=='D') vec[0]++;
+        else vec[1]++;
+        if(abs(vec[0]-vec[1])==2)break;
+    }
+    cout << vec[0] << ":" << vec[1];
+}
