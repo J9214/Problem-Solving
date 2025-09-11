0fbe758472a22fe8e3179f1187ed03ba77d8ece1
J1 <130596532+J9214@users.noreply.github.com>
2025-09-11 20:46:07 +0900

    [Silver V] Title: 복붙의 달인, Time: 16 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11008. 복붙의 달인/복붙의 달인.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<ll>> vvll;
+typedef vector<pair<int, int>> vpi;
+typedef vector<pair<ll, ll>> vpll;
+typedef struct Point {int x, y;} point;
+point direction[4] = {{1,0},{0,1},{-1,0},{0,-1}};
+#define modulo 1000000007
+void print(vvi mat){for(auto i : mat){for(auto j : i) cout << j << ' ';cout << '\n';}}
+void print(vi vec){for(auto i : vec) cout << i << ' ';}
+
+int main(){
+    //FASTIO
+
+    int t; cin >> t;
+    while(t--){
+        string str, s; cin >> str >> s;
+        int res = 0;
+        while(str.find(s,0)!=string::npos){
+            str.erase(str.begin()+str.find(s,0),str.begin()+str.find(s,0)+s.size());
+            res++;
+        }
+        cout << res + str.size() << '\n';
+    }
+    
+}
