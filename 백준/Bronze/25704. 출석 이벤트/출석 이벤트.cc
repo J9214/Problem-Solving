c5d9786f591c051fcf6681def9a4d5521cc1c2e1
J1 <130596532+J9214@users.noreply.github.com>
2025-09-25 17:21:05 +0900

    [Bronze IV] Title: 출석 이벤트, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25704. 출석 이벤트/출석 이벤트.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<ll>> vvll;
+typedef vector<pair<int, int>> vpi;
+typedef vector<pair<ll, ll>> vpll;
+typedef struct Point {int x, y;} point;
+point direction[4] = {{1,0},{0,1},{-1,0},{0,-1}};
+#define modulo 1000000007
+void print(vvi mat){for(auto i : mat){for(auto j : i) cout << j << ' ';cout << '\n';}}
+void print(vi vec){for(auto i : vec) cout << i << ' ';}
+
+int main(){
+    FASTIO
+    int n, m; cin >> n >> m;
+    int res = m;
+    if(n>=5) res = max(0,min(res, m-500));
+    if(n>=10) res = max(0,min(res, m/100*90));
+    if(n>=15) res = max(0,min(res, m-2000));
+    if(n>=20) res = max(0,min(res, m/100*75));
+
+    cout << res;
+}
