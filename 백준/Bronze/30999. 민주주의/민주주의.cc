0679084737b7999191d4d5f3472ca5ecacada2f2
J1 <130596532+J9214@users.noreply.github.com>
2025-10-04 03:20:00 +0900

    [Bronze IV] Title: 민주주의, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/30999. 민주주의/민주주의.cc
+++ 백준/Bronze/30999. 민주주의/민주주의.cc
@@ -1,29 +1,14 @@
-#include <bits/stdc++.h>
+#include<iostream>
+#include<string>
 using namespace std;
-#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-typedef long long ll;
-typedef pair<int, int> pi;
-typedef pair<ll, ll> pl;
-typedef vector<int> vi;
-typedef vector<ll> vll;
-typedef vector<vector<int>> vvi;
-typedef vector<vector<ll>> vvll;
-typedef vector<pair<int, int>> vpi;
-typedef vector<pair<ll, ll>> vpll;
-typedef struct Point {int x, y;} point;
-point direction[4] = {{1,0},{0,1},{-1,0},{0,-1}};
-#define modulo 1000000007
-void print(vvi mat){for(auto i : mat){for(auto j : i) cout << j << ' ';cout << '\n';}}
-void print(vi vec){for(auto i : vec) cout << i << ' ';}
-
 int main(){
-    FASTIO
-    
-    int n,m; cin >> n >> m;
-    int res=0;
-    while(n--){
-        string s; cin >> s;
-        if(count(s.begin(),s.end(),'O')>s.size()/2)res++;
+    int n,m,ret=0; cin>>n>>m;
+    for(int i=1;i<=n;i++){
+        string str; cin>>str;
+        int cnt=0;
+        for(int j=0;j<m;j++) if(str[j]=='O')cnt++;
+        if(cnt>=(m/2+1))ret++;
+        
     }
-    cout << res;
+    cout<<ret<<endl;
 }
