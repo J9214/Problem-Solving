7c78e4227dd3ac418d34bcc49d4cb13e2a8057ef
J1 <130596532+J9214@users.noreply.github.com>
2025-10-08 00:27:10 +0900

    [Bronze IV] Title: 홀짝홀짝, Time: 4 ms, Memory: 2860 KB -BaekjoonHub

--- 백준/Bronze/31867. 홀짝홀짝/홀짝홀짝.cc
+++ 백준/Bronze/31867. 홀짝홀짝/홀짝홀짝.cc
@@ -5,7 +5,8 @@ using namespace std;
 int main(){
     FASTIO
     int n,a=0; cin >> n;
-    string s; cin >> s;
-    for(auto i:s) a+=i%2?1:-1;
+    char c[n];
+    cin >> c;
+    for(auto i:c) a+=i%2?1:-1;
     cout << (a>0?1:a<0?0:-1);
 }
