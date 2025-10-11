9bc7e0ce008d3d32d854b58d091440b7329440b6
J1 <130596532+J9214@users.noreply.github.com>
2025-10-11 23:46:29 +0900

    [Gold II] Title: 동전 분배, Time: 340 ms, Memory: 2532 KB -BaekjoonHub

---
+++ 백준/Gold/1943. 동전 분배/동전 분배.cc
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<ll>> vvll;
+typedef vector<pair<int, int>> vpi;
+typedef vector<pair<ll, ll>> vpll;
+typedef struct Point {int x, y;} point;
+point direction[4] = {{1,0},{0,1},{-1,0},{0,-1}};
+#define modulo 1000000007
+void print(vvi mat){for(auto i : mat){for(auto j : i) cout << j << ' ';cout << '\n';}}
+void print(vi vec){for(auto i : vec) cout << i << ' ';}
+
+int main(){
+    FASTIO
+    
+    for(int t= 0; t < 3; ++t){
+        int n; cin >> n;
+        int total = 0;
+        vpi vec;
+        for(int i = 0 ; i < n ; i++){
+            int a,b; cin >> a >> b;
+            vec.push_back({a,b});
+            total += a*b;
+        }
+
+        if(total%2){cout << 0 << '\n'; continue;}
+
+        vi dp(total+1,-1);
+        dp[0] = 0;
+        for(int i = 0 ; i < n ; i++){
+            for(int j = total ; j >= 0; j--){
+                if(dp[j]!=-1) {
+                    for(int k = 1 ; k <= vec[i].second; k++){
+                        int idx = j+k*vec[i].first;
+                        if(idx <= total) dp[idx] = dp[j]+k; 
+                    }
+                }
+            }
+        }
+        
+        cout << (dp[total/2]!=-1 ? 1 : 0) << '\n';
+    }
+    
+}
