b4f9429f122de0ab08ea35e3e57e6569895f4ed9
J1 <130596532+J9214@users.noreply.github.com>
2025-10-28 00:45:36 +0900

    [Silver I] Title: 동물원, Time: 0 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/1309. 동물원/동물원.cc
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int dp[100001]={1,3,};
+int main(){
+    int n; cin>>n;
+    for(int i=2;i<=n;i++) dp[i] = (dp[i-2]*2 + dp[i-1]*2 - dp[i-2])%9901;
+    cout<<dp[n]<<'\n';
+}
