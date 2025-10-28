283c7a62766e50030a77aa7de358049d30a4d1d8
J1 <130596532+J9214@users.noreply.github.com>
2025-10-28 16:35:23 +0900

    [Silver I] Title: 동물원, Time: 0 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1309. 동물원/동물원.cc
+++ 백준/Silver/1309. 동물원/동물원.cc
@@ -1,8 +1,31 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
-int dp[100001]={1,3,};
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<ll>> vvll;
+typedef vector<pair<int, int>> vpi;
+typedef vector<pair<ll, ll>> vpll;
+typedef struct Point {int x, y;} point;
+point direction[4] = {{1,0},{0,1},{-1,0},{0,-1}};
+#define modulo 1000000007
+void print(vvi mat){for(auto i : mat){for(auto j : i) cout << j << ' ';cout << '\n';}}
+void print(vi vec){for(auto i : vec) cout << i << ' ';}
+
 int main(){
-    int n; cin>>n;
-    for(int i=2;i<=n;i++) dp[i] = (dp[i-2]*2 + dp[i-1]*2 - dp[i-2])%9901;
-    cout<<dp[n]<<'\n';
+    FASTIO
+
+    int n; cin >> n;
+    vi vec(n+1);
+    vec[0] = 1;
+    vec[1] = 3;
+    
+    for(int i = 2 ; i <= n ; i++)
+        vec[i] = (vec[i-1]*2+vec[i-2])%9901;
+    
+    cout <<vec[n];
 }
