d4088e9e39681237dccdc5104a863481e94e0ab9
J1 <130596532+J9214@users.noreply.github.com>
2025-10-30 00:21:00 +0900

    [Bronze IV] Title: 선택 가능성이 가장 높은 자료형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/32278. 선택 가능성이 가장 높은 자료형/선택 가능성이 가장 높은 자료형.cc
+++ 백준/Bronze/32278. 선택 가능성이 가장 높은 자료형/선택 가능성이 가장 높은 자료형.cc
@@ -1,26 +1,10 @@
-#include <bits/stdc++.h>
+#include<iostream>
+#include<climits>
 using namespace std;
-#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-typedef long long ll;
-typedef pair<int, int> pi;
-typedef pair<ll, ll> pl;
-typedef vector<int> vi;
-typedef vector<ll> vll;
-typedef vector<vector<int>> vvi;
-typedef vector<vector<ll>> vvll;
-typedef vector<pair<int, int>> vpi;
-typedef vector<pair<ll, ll>> vpll;
-typedef struct Point {int x, y;} point;
-point direction[4] = {{1,0},{0,1},{-1,0},{0,-1}};
-#define modulo 1000000007
-void print(vvi mat){for(auto i : mat){for(auto j : i) cout << j << ' ';cout << '\n';}}
-void print(vi vec){for(auto i : vec) cout << i << ' ';}
-
 int main(){
-    FASTIO
-
-    ll n; cin >> n;
-    if(-pow(2,15)<=n && n<pow(2,15)) cout << "short";
-    else if(-pow(2,31)<=n && n<pow(2,31)) cout << "int";
-    else cout << "long long";
+    
+    long long n; cin>>n;
+    if(-32768<=n && n<=32767)cout<<"short"<<endl;
+    else if(INT_MIN<=n && n<=INT_MAX) cout<<"int"<<endl;
+    else cout<<"long long"<<endl;       
 }
