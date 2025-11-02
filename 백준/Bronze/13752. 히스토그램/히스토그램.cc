6087175a7da66aa059179097b80688c246b18c02
J1 <130596532+J9214@users.noreply.github.com>
2025-11-03 02:08:13 +0900

    [Bronze IV] Title: 히스토그램, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/13752. 히스토그램/히스토그램.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+int main(){
+    FASTIO
+    
+    int n; cin >> n;
+    while(n--){
+        int a; cin >> a;
+        while(a--) cout << "=";
+        cout << '\n';
+    }
+}
