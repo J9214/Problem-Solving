99d34d027d941aabbc81c85e44dc7fe9c7c6b0e6
J1 <130596532+J9214@users.noreply.github.com>
2025-11-04 00:08:11 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 376 ms, Memory: 22108 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.cc
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.cc
@@ -1,46 +1,48 @@
-#include<iostream>
-#include<vector>
-#include<queue>
-#include<algorithm>
-#include<climits>
+#include <bits/stdc++.h>
 using namespace std;
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<ll>> vvll;
+typedef vector<pair<int, int>> vpi;
+typedef vector<pair<ll, ll>> vpll;
+typedef struct Point {int x, y;} point;
+point direction[4] = {{1,0},{0,1},{-1,0},{0,-1}};
+#define modulo 1000000007
+void print(vvi mat){for(auto i : mat){for(auto j : i) cout << j << ' ';cout << '\n';}}
+void print(vi vec){for(auto i : vec) cout << i << '\n';}
+
 int main(){
-    vector<vector<int>> v(300001);
-    vector<int> dist(300001,INT_MAX);
+    FASTIO
 
-    // freopen("input.txt","r",stdin);
-    int n,m,k,x,s,e; cin>>n>>m>>k>>x;
+    int n,m,k,x;cin >> n >> m >> k >> x;
+    vvi vec(n+1);
     while(m--){
-        cin>>s>>e;
-        v[s].push_back(e);
+        int a,b;cin >> a >> b;
+        vec[a].push_back(b);
     }
-    vector<int> ret;
-    queue<int> q; // 노드
-    q.push(x);
-    dist[x]=0;
-    
-    while(q.size()){
-        int cur = q.front(); 
-        int d = dist[cur];
-        q.pop();
+
+    vi res;
+    vi visit(n+1,0);
+    queue<pi> q;
+    q.push({x,0});
+
+    while(!q.empty()){
+        auto [a,b] = q.front(); q.pop();
+        if(b==k) res.push_back(a);
         
-        for(int i=0;i<v[cur].size();i++){
-            int next = v[cur][i];
-            if(dist[next]>d+1){
-                q.push(next);
-                dist[next]=d+1;
-            }
+        for(int i = 0 ; i < vec[a].size(); i++){
+            if(visit[vec[a][i]]||vec[a][i]==x) continue;
+            visit[vec[a][i]] = b+1;
+            q.push({vec[a][i],b+1});
         }
     }
 
-    bool is = false;
-    for(int i=1;i<=n;i++){
-        if(dist[i]==k){
-            cout<<i<<'\n';
-            is = 1;
-        }
-    }
-    if(is)exit(0);
-    cout<<-1<<endl;
-
+    sort(res.begin(), res.end());
+    if(res.size()) print(res);
+    else cout << -1;
 }
