a48d2ad9913e32c535bcd034069bc173d5f68174
J1 <130596532+J9214@users.noreply.github.com>
2025-11-05 01:03:06 +0900

    [Silver II] Title: 이동하기, Time: 200 ms, Memory: 5936 KB -BaekjoonHub

---
+++ 백준/Silver/11048. 이동하기/이동하기.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<queue>
+#include<algorithm>
+using namespace std;
+
+int dp[1001][1001]={0,};
+int n,m;
+int main(){
+    cin>>n>>m;
+    for(int i=1;i<=n;i++)for(int j=1;j<=m;j++){
+        cin>>dp[i][j];
+        dp[i][j] += max(dp[i-1][j],max(dp[i][j-1],dp[i-1][j-1]));
+    }
+    cout<<dp[n][m]<<'\n';
+    
+}
