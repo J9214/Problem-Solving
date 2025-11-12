e040750964fa9affcc9b198655a8f74fcd424c4a
J1 <130596532+J9214@users.noreply.github.com>
2025-11-12 12:28:15 +0900

    [Silver I] Title: 뒤집기 II, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1455. 뒤집기 II/뒤집기 II.cc
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<ll>> vvll;
+typedef vector<pair<int, int>> vpi;
+typedef vector<pair<ll, ll>> vpll;
+typedef struct Point {int x, y;} point;
+point direction[4] = {{1,0},{0,1},{-1,0},{0,-1}};
+#define modulo 1000000007
+void print(vvi mat){for(auto i : mat){for(auto j : i) cout << j << ' ';cout << '\n';}}
+void print(vi vec){for(auto i : vec) cout << i << ' ';}
+
+int n,m; 
+vector<vector<bool>> vec;
+
+int res=0;
+
+void reverse(int x, int y){
+    res++;
+    for(int i = 0; i <= x; i++)
+        for(int j = 0 ; j <= y; j++)
+            vec[i][j] = !vec[i][j];
+}
+
+void check(){
+    int x=n-1;
+    int y=m-1;
+
+    for(int i = x; i >= 0; i--){
+        for(int j = y; j >= 0; j--){
+            if(vec[i][j]) reverse(i,j);
+        }
+    }
+}
+
+int main(){
+    FASTIO
+
+    cin >> n>>m;
+    vec.resize(n, vector<bool>(m));
+    for(int i = 0 ; i < n ; i ++) for(int j = 0 ; j < m ; j++) {
+        char c; cin >> c;
+        vec[i][j] = c-'0';
+    }
+
+    check();
+
+    cout << res;
+}
