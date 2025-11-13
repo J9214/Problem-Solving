0cb7e162398e4e6492a9588789663bc1ecf9f9ca
J1 <130596532+J9214@users.noreply.github.com>
2025-11-13 23:28:34 +0900

    [Silver II] Title: 친구, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/1058. 친구/친구.cc
+++ 백준/Silver/1058. 친구/친구.cc
@@ -1,30 +1,50 @@
-#include<iostream>
-#include<string>
+#include <bits/stdc++.h>
 using namespace std;
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<ll>> vvll;
+typedef vector<pair<int, int>> vpi;
+typedef vector<pair<ll, ll>> vpll;
+typedef struct Point {int x, y;} point;
+point direction[4] = {{1,0},{0,1},{-1,0},{0,-1}};
+#define modulo 1000000007
+void print(vvi mat){for(auto i : mat){for(auto j : i) cout << j << ' ';cout << '\n';}}
+void print(vi vec){for(auto i : vec) cout << i << ' ';}
+
+vvi mat_mult(vvi vec1, vvi vec2, long long n){
+    vvi vec(n, vector<int>(n, 0));
+
+    for(int i = 0 ; i < n; i++) for(int j = 0 ; j < n ; j++) for(int k = 0 ; k < n ; k++)
+        vec[i][j] += vec1[i][k] * vec2[k][j];
+
+    return vec;
+}
 
 int main(){
-    ios::sync_with_stdio(0);cin.tie(0);
-    string str[50];
-    // freopen("input.txt","r",stdin);
-    int n; cin>>n;
-    for(int i=0;i<n;i++)cin>>str[i];
-    int ret=0;
-    for(int i=0;i<n;i++){
-        int v[50]={0,};
-        int cur=0;
-        for(int j=0;j<n;j++){
-            // 직속 친구
-            if(str[i][j]=='Y' && !v[j]){v[j]=1; cur++; }  
+    FASTIO
 
-            // 한다리 건너 친구 
-            if(str[j][i]=='Y'){
-                for(int k=0;k<n;k++){
-                    if(k==i) continue;
-                    if(str[j][k]=='Y' && !v[k]){ v[k]=1; cur++;}
-                }
-            }
+    int n; cin >> n;
+    vvi mat(n, vi(n,0));
+
+    for(int i = 0 ; i < n ; i ++){
+        for(int j = 0 ; j < n ; j++){
+            char c; cin >> c;
+            if(c=='Y') mat[i][j] = 1;
         }
-        ret=max(ret,cur);
     }
-    cout<<ret<<'\n';
+    vvi check = mat;
+    ll res=0;
+    mat = mat_mult(mat, mat, n);
+    for(int i = 0 ; i < n ; i++){
+        for(int j = 0 ; j < n ; j++){
+            if(mat[i][j]) check[i][j] = 1;
+        }
+    }
+    for(auto v : check) res = max(res, (ll)count(v.begin(),v.end(),1)-1);
+    cout << res;
 }
