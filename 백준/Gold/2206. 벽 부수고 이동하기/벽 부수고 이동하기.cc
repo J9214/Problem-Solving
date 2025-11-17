e786518108d8ec8752bf7480380436ed8859f913
J1 <130596532+J9214@users.noreply.github.com>
2025-11-17 12:57:27 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 84 ms, Memory: 18452 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cc
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cc
@@ -9,32 +9,26 @@ typedef vector<pair<int, int>> vpi;
 typedef struct Point {int x, y;} point;
 point direction[4] = {{1,0},{0,1},{-1,0},{0,-1}};
 
-int n, m, res;
-vvi vec,t_vec;
-vvi wall;
-vvi visited, t_visited;
+int n, m, res=99999999; 
 
 int main(){
     FASTIO
 
     cin >> n >> m;
-    res = 99999999;
-    vec.assign(n,vi(m,0));
-    t_vec.assign(n,vi(m,0));
-    wall.resize(n, vi(m));
-    visited.assign(n,vi(m,0));
+    int vec[n][m][4] = {};
+
     for(int i = 0 ; i < n ; i++){
         for(int j = 0 ; j < m ; j++){
             char c; cin >> c;
-            wall[i][j] = {c-'0'};
+            vec[i][j][2]= {c-'0'};
         }
     }
 
     queue<point> q;
     q.push({0,0});
 
-    vec[0][0]= 1;
-    visited[0][0] = 1;
+    vec[0][0][0]= 1;
+    vec[0][0][3] = 1;
 
     while(q.size()){
         auto [x,y] = q.front(); q.pop();
@@ -42,35 +36,36 @@ int main(){
         for(auto dir : direction){
             int nx = x + dir.x;
             int ny = y + dir.y;
-            if(nx<0||ny<0||nx>=n||ny>=m||visited[nx][ny]) continue;
+            if(nx<0||ny<0||nx>=n||ny>=m||vec[nx][ny][3]) continue;
 
-            visited[nx][ny] = 1;
-            vec[nx][ny] = vec[x][y] + 1;
-            if(!wall[nx][ny]) q.push({nx,ny});
+            vec[nx][ny][3] = 1;
+            vec[nx][ny][0] = vec[x][y][0] + 1;
+            if(!vec[nx][ny][2]) q.push({nx,ny});
         }
     }
     
     q.push({n-1,m-1});
 
-    t_vec[n-1][m-1] = 1;
-    visited[n-1][m-1] = 2;
+    vec[n-1][m-1][1] = 1;
+    vec[n-1][m-1][3] = 2;
+
     while(q.size()){
         auto [x,y] = q.front(); q.pop();
 
         for(auto dir : direction){
             int nx = x + dir.x;
             int ny = y + dir.y;
-            if(nx<0||ny<0||nx>=n||ny>=m||visited[nx][ny]==2) continue;
+            if(nx<0||ny<0||nx>=n||ny>=m||vec[nx][ny][3]==2) continue;
 
-            visited[nx][ny] = 2;
-            t_vec[nx][ny] = t_vec[x][y] + 1;
-            if(!wall[nx][ny]) q.push({nx,ny});
+            vec[nx][ny][3] = 2;
+            vec[nx][ny][1] = vec[x][y][1] + 1;
+            if(!vec[nx][ny][2]) q.push({nx,ny});
         }
     }
 
     for(int i = 0 ; i < n ; i ++)
         for(int j = 0 ; j < m ; j++)
-            if(vec[i][j] && t_vec[i][j]) res = min(res, vec[i][j]+t_vec[i][j]-1);
+            if(vec[i][j][0] && vec[i][j][1]) res = min(res, vec[i][j][0]+vec[i][j][1]-1);
 
     if(res!=99999999) cout << res;
     else cout << -1;
