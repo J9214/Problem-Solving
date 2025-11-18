82f04856f26df034eeeccc92201f880aec1e4c26
J1 <130596532+J9214@users.noreply.github.com>
2025-11-18 11:38:40 +0900

    [Silver V] Title: 크기가 4인 박스, Time: 8 ms, Memory: 2416 KB -BaekjoonHub

---
+++ 백준/Silver/34704. 크기가 4인 박스/크기가 4인 박스.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<ll>> vvll;
+typedef vector<pair<int, int>> vpi;
+typedef vector<pair<ll, ll>> vpll;
+typedef struct Point {int x, y;} point;
+point direction[4] = {{1,0},{0,1},{-1,0},{0,-1}};
+#define modulo 1000000007
+void print(vvi mat){for(auto i : mat){for(auto j : i) cout << j << ' ';cout << '\n';}}
+void print(vi vec){for(auto i : vec) cout << i << ' ';}
+
+int main(){
+    FASTIO
+    int n; cin >> n;
+    vi vec(n);
+    for(auto &i :vec) cin>>i;
+    sort(vec.begin(),vec.end());
+    
+    int i = 0, j = n-1;
+    while(i<j){
+        if(vec[j]+vec[i] <= 4) vec[j]+=vec[i++];
+        else j--;
+    }
+
+    cout << n-j;
+}
