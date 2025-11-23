47d8324746c9ed3f66be6f8c88cd96673d27b01f
J1 <130596532+J9214@users.noreply.github.com>
2025-11-24 02:42:32 +0900

    [Bronze IV] Title: 타일 채우기 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15700. 타일 채우기 4/타일 채우기 4.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+int main(){
+    FASTIO
+
+    long long n, m; cin >> n >> m;
+
+    cout << n*m/2;
+}
