45d0bfc9dbd14181ff3f79e957711a2e17f67c44
J1 <130596532+J9214@users.noreply.github.com>
2025-11-26 20:55:07 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<ll>> vvll;
+typedef vector<pair<int, int>> vpi;
+typedef vector<pair<ll, ll>> vpll;
+typedef struct Point {int x, y;} point;
+point direction[4] = {{1,0},{0,1},{-1,0},{0,-1}};
+#define modulo 1000000007
+void print(vvi mat){for(auto i : mat){for(auto j : i) cout << j << ' ';cout << '\n';}}
+void print(vi vec){for(auto i : vec) cout << i << ' ';}
+
+int main(){
+    FASTIO
+    
+    int n; cin >> n;
+    while(n--){
+        map<string, int> mp;
+        int m; cin >> m;
+        while(m--){
+            string a,b;cin >> a >> b;
+            mp[b]++;
+        }
+        int res = 1;
+        for(auto i : mp) res *= i.second+1;
+        cout << res-1 << '\n';
+    }
+}
