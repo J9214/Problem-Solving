ec46af61a55d43eb74478af8821a37a9e175a6fa
J1 <130596532+J9214@users.noreply.github.com>
2025-12-02 21:10:53 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.cc
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.cc
@@ -9,33 +9,24 @@ vector<Info> list;
 int n;
 int ret= INT_MAX;
 
-void Solution(int v, int d){
+void Solution(int d,int t_s, int t_b){
+    if(t_b) ret=min(abs(t_s-t_b),ret);
+    
     //base
-    if(d>=n){
-        if(!v) return; // 하나도 안넣었으면 패쓰
-        int total_s=1,total_b=0;
-        for(int i=0;i<n;i++){
-            if((v>>i)&1){
-                total_s *=list[i].s;
-                total_b +=list[i].b;
-            }
-        }
-        ret=min(abs(total_s-total_b),ret);
-        return;
-    }
+    if(d==n) return;
 
     //recursive
-    Solution(v|(1<<d),d+1);// 넣는 경우
-    Solution(v,d+1); // 안넣는 경우
+    Solution(d+1,t_s*list[d].s,t_b+list[d].b);// 넣는 경우
+    Solution(d+1,t_s,t_b); // 안넣는 경우
 }
 int main(){
     ios::sync_with_stdio(0);cin.tie(0);
-    
+
     cin>>n;
     for(int i=0,a,b;i<n;i++){
         cin>>a>>b;
         list.push_back({a,b});
     }
-    Solution(0,0);
+    Solution(0,1,0);
     cout<<ret<<'\n';
 }
