e8ab27144e03a21011c4af5d15bbf90d349891c2
J1 <130596532+J9214@users.noreply.github.com>
2025-12-06 00:48:55 +0900

    [Bronze IV] Title: 구구단표, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32710. 구구단표/구구단표.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<ll>> vvll;
+typedef vector<pair<int, int>> vpi;
+typedef vector<pair<ll, ll>> vpll;
+typedef struct Point {int x, y;} point;
+point direction[4] = {{1,0},{0,1},{-1,0},{0,-1}};
+#define modulo 1000000007
+void print(vvi mat){for(auto i : mat){for(auto j : i) cout << j << ' ';cout << '\n';}}
+void print(vi vec){for(auto i : vec) cout << i << ' ';}
+
+int main(){
+    FASTIO
+
+    int n; cin >> n;
+    for(int i = 2; i < 10 ; i++)
+        for(int j = 1; j < 10 ; j++)
+            if(n==i||n==j||n==i*j) {cout << 1; return 0;}
+    cout << 0;
+}
