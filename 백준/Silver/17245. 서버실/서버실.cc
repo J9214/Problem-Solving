548a48a03e81b186b0c4d4ffc530da386dd7dbd1
J1 <130596532+J9214@users.noreply.github.com>
2025-12-08 23:56:55 +0900

    [Silver II] Title: 서버실, Time: 208 ms, Memory: 14440 KB -BaekjoonHub

--- 백준/Silver/17245. 서버실/서버실.cc
+++ 백준/Silver/17245. 서버실/서버실.cc
@@ -1,54 +1,29 @@
-#include <bits/stdc++.h>
+#include<iostream>
+#include<algorithm>
+#include<vector>
 using namespace std;
-#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-typedef long long ll;
-typedef pair<int, int> pi;
-typedef pair<ll, ll> pl;
-typedef vector<int> vi;
-typedef vector<ll> vll;
-typedef vector<vector<int>> vvi;
-typedef vector<vector<ll>> vvll;
-typedef vector<pair<int, int>> vpi;
-typedef vector<pair<ll, ll>> vpll;
-typedef struct Point {int x, y;} point;
-point direction[4] = {{1,0},{0,1},{-1,0},{0,-1}};
-#define modulo 1000000007
-void print(vvi mat){for(auto i : mat){for(auto j : i) cout << j << ' ';cout << '\n';}}
-void print(vi vec){for(auto i : vec) cout << i << ' ';}
-void print(vll vec){for(auto i : vec) cout << i << ' ';}
-void print(vpi vec){for(auto i : vec) cout << i.first << ' ' << i.second << '\n';}
 
+vector<long long> v;
 int main(){
-    FASTIO
+    ios::sync_with_stdio(0);cin.tie(0);
+    long long n; cin>>n;
     
-    ll n; cin >> n;
-    ll total=0;
-    map<int,int> mp;
-
-    for(int i = 0 ; i < n ; i++)
-        for(int j = 0 ; j < n ; j++){
-            int d;cin >>d;
-            mp[d]++;
-            total += d;
-        }
+    long long s=0;
     
-    int com=n*n;
-    int prev = 0;
-
-    ll need = (total + 1) / 2;
-
-    for (auto &[h, cnt] : mp) {
-        ll len = h - prev;         
-        ll can_add = len * com;    
-
-        if (need <= can_add) {        
-            ll delta = (need + com - 1) / com; 
-            cout << prev + delta;
-            return 0;
-        }
-
-        need -= can_add;  
-        com -= cnt;       
-        prev = h;
+    for(long long i=0,tmp;i<n*n;i++){
+        cin>>tmp;
+        if(!tmp) continue;
+        v.push_back(tmp);
+        s+=tmp;
     }
+    sort(v.begin(),v.end());
+
+    long long ret=0,cur=0,idx=0;
+    while(cur*2<s){
+        cur += v.size()-idx;
+        ret++;
+        auto it = upper_bound(v.begin(),v.end(),ret);
+        idx = it-v.begin();
+    }
+    cout<<ret<<'\n';
 }
