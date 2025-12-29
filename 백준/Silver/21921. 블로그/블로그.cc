d2db5931ae1277da74bd2e1b91d2d70accde951b
J1 <130596532+J9214@users.noreply.github.com>
2025-12-29 23:08:59 +0900

    [Silver III] Title: 블로그, Time: 20 ms, Memory: 3000 KB -BaekjoonHub

--- 백준/Silver/21921. 블로그/블로그.cc
+++ 백준/Silver/21921. 블로그/블로그.cc
@@ -1,26 +1,42 @@
-#include<iostream>
-#include<algorithm>
-#define SIZE 250'000
+#include <bits/stdc++.h>
 using namespace std;
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<ll>> vvll;
+typedef vector<pair<int, int>> vpi;
+typedef vector<pair<ll, ll>> vpll;
+typedef struct Point {int x, y;} point;
+point direction[4] = {{1,0},{0,1},{-1,0},{0,-1}};
+#define modulo 1000000007
+void print(vvi mat){for(auto i : mat){for(auto j : i) cout << j << ' ';cout << '\n';}}
+void print(vi vec){for(auto i : vec) cout << i << ' ';}
 
-long long list[SIZE]={0,};
 int main(){
-    ios::sync_with_stdio(0); cin.tie(0);
-    int n,x; cin>>n>>x;
-    long long tmp,ret=0,cnt=0;
-    for(int i=1,tmp;i<=n;i++){
-        cin>>tmp;
-        list[i]=list[i-1]+tmp;
+    FASTIO
+ 
+    int n, m; cin >> n >> m;
+    vi vec(n);
+    for(auto &i : vec) cin >> i;
+    int res=0;
+    int day=0;
+;    int t=0;
+
+    for(int i = 0 ; i < n ; i++){
+        t+=vec[i];
+        if(i>=m) t -= vec[i-m];
+
+        if(res == t) day++;
+        else if(res<t)day = 1;
         
-        if(i>=x){
-            tmp = list[i]-list[i-x];
-            if(tmp>ret){
-                ret=tmp;
-                cnt=1;
-            }
-            else if(tmp==ret) cnt++;
-        }
+        res = max(res, t);
     }
-    if(ret) cout<<ret<<'\n'<<cnt<<'\n';
-    else cout<<"SAD"<<'\n';
+
+
+    if(res == 0) cout << "SAD";
+    else cout << res << '\n' << day;
 }
