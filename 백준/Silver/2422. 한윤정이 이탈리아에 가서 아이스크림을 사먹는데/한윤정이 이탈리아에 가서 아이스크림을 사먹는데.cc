1c36b7e20bfdea2e5ce978bb308def2fecf0c37d
J1 <130596532+J9214@users.noreply.github.com>
2026-01-08 22:34:25 +0900

    [Silver IV] Title: 한윤정이 이탈리아에 가서 아이스크림을 사먹는데, Time: 100 ms, Memory: 2284 KB -BaekjoonHub

--- 백준/Silver/2422. 한윤정이 이탈리아에 가서 아이스크림을 사먹는데/한윤정이 이탈리아에 가서 아이스크림을 사먹는데.cc
+++ 백준/Silver/2422. 한윤정이 이탈리아에 가서 아이스크림을 사먹는데/한윤정이 이탈리아에 가서 아이스크림을 사먹는데.cc
@@ -1,42 +1,27 @@
-#include <bits/stdc++.h>
+#include<iostream>
+#include<vector>
+#define SIZE 200
 using namespace std;
-#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-typedef long long ll;
-typedef pair<int, int> pi;
-typedef pair<ll, ll> pl;
-typedef vector<int> vi;
-typedef vector<ll> vll;
-typedef vector<vector<int>> vvi;
-typedef vector<vector<ll>> vvll;
-typedef vector<pair<int, int>> vpi;
-typedef vector<pair<ll, ll>> vpll;
-typedef struct Point {int x, y;} point;
-point direction[4] = {{1,0},{0,1},{-1,0},{0,-1}};
-#define modulo 1000000007
-void print(vvi mat){for(auto i : mat){for(auto j : i) cout << j << ' ';cout << '\n';}}
-void print(vi vec){for(auto i : vec) cout << i << ' ';}
+
+vector<vector<int>> list(201);
+
+bool Check(int a,int b,int c){
+    for(int i=0;i<list[a].size();i++) if(list[a][i]==b || list[a][i]==c) return 0;
+    return 1;
+}
 
 int main(){
-    FASTIO
+    ios::sync_with_stdio(0);cin.tie(0);
+    int n,m;cin>>n>>m;
     
-    int n; cin >> n;
-    vvi vec(n+1, vi(n+1,0));
-    int m; cin >> m;
-    while(m--){
-        int a,b;cin >> a>> b;
-        vec[a][b]=vec[b][a] =1;
+    for(int i=0,a,b;i<m;i++){
+        cin>>a>>b;
+        list[a].push_back(b);
+        list[b].push_back(a);
     }
 
-    int res =0;
-    for(int i = 1 ; i <= n ; i++){
-        for(int j = i+1; j <= n ; j++){
-            if(vec[i][j]) continue;
-            for(int k = j +1; k<=n; k++){
-                if(vec[i][k]||vec[j][k]) continue;
-                res++;
-            }
-        }
-    }
-
-    cout << res;
+    int ret=0;
+    for(int i=1;i<=n-2;i++)for(int j=i+1;j<=n-1;j++)for(int k=j+1;k<=n;k++)
+        if(Check(i,j,k) && Check(j,i,k) && Check(k,i,j)) ret++;
+    cout<<ret<<'\n';
 }
