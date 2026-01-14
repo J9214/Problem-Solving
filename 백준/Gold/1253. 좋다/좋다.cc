bab617627363d0ee3a2b4b4c1936f252e227e14d
J1 <130596532+J9214@users.noreply.github.com>
2026-01-14 10:17:30 +0900

    [Gold IV] Title: 좋다, Time: 20 ms, Memory: 2160 KB -BaekjoonHub

---
+++ 백준/Gold/1253. 좋다/좋다.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<ll>> vvll;
+typedef vector<pair<int, int>> vpi;
+typedef vector<pair<ll, ll>> vpll;
+typedef struct Point {int x, y;} point;
+point direction[4] = {{1,0},{0,1},{-1,0},{0,-1}};
+#define modulo 1000000007
+void print(vvi mat){for(auto i : mat){for(auto j : i) cout << j << ' ';cout << '\n';}}
+void print(vi vec){for(auto i : vec) cout << i << ' ';}
+
+int main(){
+    FASTIO
+
+    int n; cin >> n;
+    vll vec(n);
+
+    for(auto &i : vec) cin >> i;
+    
+    sort(vec.begin(),vec.end());
+
+    ll res = 0;
+
+    for(int i = 0 ; i < n; i++){
+        int j = 0, k = n-1;
+        while(j<k){
+            if(j==i) {j++; continue;}
+            if(i==k) {k--; continue;}
+            if(vec[j]+vec[k]==vec[i]) {res++; break;}
+            if(vec[j]+vec[k]<vec[i]) j++;
+            else k--;
+        }
+    }
+    cout << res;
+} 
