741ac59995afd6c6211dd7b5b2d0a84dc8c60540
J1 <130596532+J9214@users.noreply.github.com>
2026-01-17 01:06:45 +0900

    [Bronze IV] Title: 왜 맘대로 예약하냐고, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32941. 왜 맘대로 예약하냐고/왜 맘대로 예약하냐고.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<ll>> vvll;
+typedef vector<pair<int, int>> vpi;
+typedef vector<pair<ll, ll>> vpll;
+typedef struct Point {int x, y;} point;
+point direction[4] = {{1,0},{0,1},{-1,0},{0,-1}};
+#define modulo 1000000007
+void print(vvi mat){for(auto i : mat){for(auto j : i) cout << j << ' ';cout << '\n';}}
+void print(vi vec){for(auto i : vec) cout << i << ' ';}
+
+int main(){
+    FASTIO
+
+    int n, m; cin >> n>> m;
+    int k; cin >> k;
+    vi vec(n+1);
+    for(int i = 0 ; i < k ; i++){
+        int d; cin >>d;
+        while(d--){
+            int a; cin >> a;
+            vec[a]++;
+        }
+    }
+
+    if(vec[m]==k) cout << "YES";
+    else cout << "NO";
+}
