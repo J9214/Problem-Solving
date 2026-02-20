e28949c8cbcd47c38f290d3736ae1e952f5cf341
J1 <130596532+J9214@users.noreply.github.com>
2026-02-21 01:02:05 +0900

    [Bronze IV] Title: Contemporary Art, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/16600. Contemporary Art/Contemporary Art.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+int main(){
+    FASTIO
+    
+    double d; cin >> d;
+    
+    cout.precision(6);
+    cout<<fixed<<sqrt(d)*4;
+}
