4f934cd3e4e52c2dcbb61fc0fee10e2820d50099
J1 <130596532+J9214@users.noreply.github.com>
2026-02-27 14:21:06 +0900

    [Silver II] Title: 창고 다각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<ll>> vvll;
+typedef vector<pair<int, int>> vpi;
+typedef vector<pair<ll, ll>> vpll;
+typedef struct Point {int x, y;} point;
+point direction[4] = {{1,0},{0,1},{-1,0},{0,-1}};
+#define modulo 1000000007
+void print(vvi mat){for(auto i : mat){for(auto j : i) cout << j << ' ';cout << '\n';}}
+void print(vi vec){for(auto i : vec) cout << i << ' ';}
+
+int main(){
+    FASTIO
+ 
+    int n; cin >> n;
+    point m={0,0};
+    vi v(1002,0);
+
+    for(int i = 0;  i < n ; i++) {
+        point a; cin >> a.x >> a.y;
+        v[a.x] = a.y;
+        m = a.y>m.y?a:m;
+    }
+
+    for(int i = 1; i <= m.x; i++) v[i] = max(v[i-1],v[i]);
+    for(int i = 1000; i >= m.x; i--) v[i] = max(v[i+1],v[i]);
+    cout << accumulate(v.begin(),v.end(),0);
+}
