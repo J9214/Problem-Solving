fe0bcc128539a7a1cd9ca13d54aedf295b0c63a3
J1 <130596532+J9214@users.noreply.github.com>
2026-03-07 19:44:00 +0900

    [Gold V] Title: 선발 명단, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/3980. 선발 명단/선발 명단.cc
+++ 백준/Gold/3980. 선발 명단/선발 명단.cc
@@ -1,53 +1,51 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
-#define SIZE 11
-using namespace std;
+    #include <bits/stdc++.h>
+    using namespace std;
+    #define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    typedef long long ll;
+    typedef pair<int, int> pi;
+    typedef pair<ll, ll> pl;
+    typedef vector<int> vi;
+    typedef vector<ll> vll;
+    typedef vector<vector<int>> vvi;
+    typedef vector<vector<ll>> vvll;
+    typedef vector<pair<int, int>> vpi;
+    typedef vector<pair<ll, ll>> vpll;
+    typedef struct Point {int x, y;} point;
+    point direction[4] = {{1,0},{0,1},{-1,0},{0,-1}};
+    #define modulo 1000000007
+    void print(vvi mat){for(auto i : mat){for(auto j : i) cout << j << ' ';cout << '\n';}}
+    void print(vi vec){for(auto i : vec) cout << i << ' ';}
 
-struct info{int s,n;}; // 실력, 몇번 선수? 
-bool cmp(const info& a, const info& b){return a.s>b.s;};
-
-vector<vector<info>> graph(SIZE); // 포지션별 
-
-int ret=0;
-// v : 몇번 선수를 썼는지
-// pos: 몇번째 포지션을 결정중인지
-void DFS(int pos, int sum, int v){ 
-    // base
-    if(pos==11){
-        ret=max(ret,sum);
-        return;
-    }
-
-    // recursive
-    for(int i=0;i<graph[pos].size();i++){
-        info cur = graph[pos][i];
-        if((v>>cur.n)&1) continue; 
-        DFS(pos+1,sum+cur.s,v|(1<<cur.n));
-    }
-}
-
-int main(){
-    ios::sync_with_stdio(0);cin.tie(0);
-
-    // 각 테케에 대하여
-    int c; cin>>c;
-    while(c--){
-        // graph 초기화
-        for(int i=0;i<SIZE;i++)graph[i].clear();
-        
-        // 정보 입력받기 
-        int a;
-        for(int i=0;i<SIZE;i++)for(int j=0;j<SIZE;j++){
-            cin>>a;
-            if(a) graph[j].push_back({a,i});
+    vi check(11,0);
+    vector<vector<pi>> vec(11); 
+    int m;
+    void func(int s,int num){ 
+        if(num==11){
+            m=max(m,s);
+            return;
         }
 
-        // 각 포지션별 실력 내림차순
-        for(int i=0;i<SIZE;i++) sort(graph[i].begin(),graph[i].end(),cmp);
-        
-        ret=0; // 결과 초기화
-        DFS(0,0,0);
-        cout<<ret<<'\n';
+        for(int i = 0 ; i < vec[num].size() ; i++){
+            if(check[vec[num][i].first]) continue;
+            check[vec[num][i].first]=1;
+            func(s+vec[num][i].second,num+1);
+            check[vec[num][i].first]=0;
+        }
     }
-}
+    int main(){
+        FASTIO
+    
+        int t; cin >> t;
+        while(t--){
+            check.assign(11,0);
+            vec.clear();
+            vec.resize(11);
+            m=0;
+            for(int i = 0 ; i < 11; i++)for(int j = 0 ; j < 11; j++){
+                int d; cin >> d;
+                if(d) vec[j].push_back({i,d});
+            }
+            func(0,0);
+            cout << m <<'\n';
+        }
+    }
