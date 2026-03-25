325af90fb2315518ac0c9931c2d445e4cc1a01ac
J1 <130596532+J9214@users.noreply.github.com>
2026-03-26 02:07:12 +0900

    [Bronze III] Title: 별 찍기 - 16, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10991. 별 찍기 － 16/별 찍기 － 16.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<ll>> vvll;
+typedef vector<pair<int, int>> vpi;
+typedef vector<pair<ll, ll>> vpll;
+typedef struct Point {int x, y;} point;
+point direction[4] = {{1,0},{0,1},{-1,0},{0,-1}};
+#define modulo 1000000007
+
+int main(){
+    FASTIO
+
+    int n; cin >> n;
+    for(int i = n - 1; i >= 0; i--){
+        for(int j = 0 ; j < i; j++) cout << ' ';
+        for(int j = 0; j <n-i; j++) cout << "* ";
+        cout << '\n';
+    }
+}
