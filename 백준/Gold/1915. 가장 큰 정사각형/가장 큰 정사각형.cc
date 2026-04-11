8e2a555546d8acada65ebf32e20909dbc443c735
J1 <130596532+J9214@users.noreply.github.com>
2026-04-12 03:54:56 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 16 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.cc
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.cc
@@ -1,37 +1,25 @@
-#include <bits/stdc++.h>
+#include<iostream>
+#include<algorithm>
+#define SIZE 1000
 using namespace std;
-#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-typedef long long ll;
-typedef pair<int, int> pi;
-typedef pair<ll, ll> pl;
-typedef vector<int> vi;
-typedef vector<ll> vll;
-typedef vector<vector<int>> vvi;
-typedef vector<vector<ll>> vvll;
-typedef vector<pair<int, int>> vpi;
-typedef vector<pair<ll, ll>> vpll;
-typedef struct Point {int x, y;} point;
-point direction[4] = {{1,0},{0,1},{-1,0},{0,-1}};
-#define modulo 1000000007
-void print(vvi mat){for(auto i : mat){for(auto j : i) cout << j << ' ';cout << '\n';}}
-void print(vi vec){for(auto i : vec) cout << i << ' ';}
 
+int arr[SIZE][SIZE]={0,};
 int main(){
-    FASTIO
- 
-    int n, m, res=0; cin >> n >> m;
-    vvi vec(n,vi(m));
+    ios::sync_with_stdio(0);cin.tie(0);
 
-    for(int i = 0 ; i < n ; i++){
-        for(int j = 0; j < m; j++){
-            char c; cin >> c;
-            vec[i][j]=c-'0';
+    int n,m,ret=0;; cin>>n>>m;
 
-            if(!vec[i][j]) continue;
+    char c; 
+    for(int i=0;i<n;i++)for(int j=0;j<m;j++){
+        cin>>c;
+        arr[i][j]=c-'0';
+        ret=max(arr[i][j],ret);
+        if(!arr[i][j]) continue;
 
-            int f = min({(i?vec[i-1][j]:0), (i&&j?vec[i-1][j-1]:0), (j?vec[i][j-1]:0)});
-            res = max(res,vec[i][j] = pow(sqrt(f)+1,2));
+        // 위, 왼쪽, 대각선 
+        if(0<i && arr[i-1][j] && 0<j && arr[i][j-1] && arr[i-1][j-1]){
+            ret=max(ret,arr[i][j]=min(arr[i-1][j],min(arr[i][j-1],arr[i-1][j-1]))+1);
         }
     }
-    cout << res;
+    cout<<ret*ret<<'\n';
 }
