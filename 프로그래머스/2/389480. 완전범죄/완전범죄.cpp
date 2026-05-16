a08441233c583b0161969395f799362f943de62e
J1 <130596532+J9214@users.noreply.github.com>
2026-05-16 14:42:42 +0900

    [level 2] Title: 완전범죄, Time: 9.05 ms, Memory: 5.23 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.cpp
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.cpp
@@ -1,36 +1,24 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <set>
 
 using namespace std;
 
 int res = 9999999;
-struct st{
-    int a,b,c;
-    bool operator<(const st &other) const{
-        if(a==other.a){
-            if(b==other.b) return c<other.c;
-            return b<other.b;
-        }
-        return a<other.a;
-    }
-};
-set<st> s;
+vector<int> memo;
 void recur(vector<vector<int>> &info, int n, int m, int idx, int a, int b){
-    if(a>=n || b>=m) return;
+    if(a>=n || b>=m || a >= memo[idx]) return;
     if(idx == info.size()) {
         res = min(res, a);
         return;
     }
-    if(s.find({idx,a,b})!=s.end()) return;
-
-    s.insert({idx,a,b});
     
+    memo[idx] = min(memo[idx], a);
     recur(info,n,m,idx+1, a+info[idx][0],b);
     recur(info,n,m,idx+1, a,b+info[idx][1]);
 }
 int solution(vector<vector<int>> info, int n, int m) {
+    memo.assign(info.size(), 9999999);
     recur(info,n,m,0,0,0);
     if(res>=n) return -1;
     return res;
